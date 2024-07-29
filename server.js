ee516577e324ebaccfe6eba5594cb49f4836a7be
Cadecraft <cadendotlee@gmail.com>
2024-07-29 13:24:35 -0700

    feat: initial files

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const http = require("http");
+const fs = require("fs");
+const path = require("fs");
 const WebSocketServer = require("websocket").server;
 var sids = [];
 var clients = {};
-const httpServer = http.createServer()
+let indexContents = fs.readFileSync("./index.html");
+const httpServer = http.createServer(function (req, res) {
+	// Write a response
+	res.writeHeader(200, { "Content-type": "text/html" });
+	res.write(indexContents);
+	res.end();
+})
 const ws = new WebSocketServer({"httpServer":httpServer});
 const PORT = 5005;
 httpServer.listen(PORT, "127.0.0.1", () => console.log(`Server is open on port : ${PORT}`));
