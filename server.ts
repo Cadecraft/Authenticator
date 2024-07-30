7e0d21cc6488e91751d4ac00ee0671602405009e
Cadecraft <cadendotlee@gmail.com>
2024-07-30 08:35:02 -0700

    feat: ts config stuff

--- server.ts
+++ server.ts
@@ -1,15 +1,20 @@
 // Requires
-import http = require("http");
-import fs = require("fs");
-import path = require("fs");
-import websocket = require("websocket");
+//import http = require("http");
+import * as http from "http";
+//import fs = require("fs");
+import * as fs from "fs";
+//import path = require("fs");
+//import websocket = require("websocket");
+//import * as mywasm from "./wasm_modules/pkg"; // Include final `/`?
 //import server as WebSocketServer from "websocket";
+// Needed this to be the JS for node to be happy:
+//var mywasm = import("./wasm_modules/pkg/wasm_modules");
 
 // Information
 // TODO: start storing info
 
 // Serving contents and setup
-let indexContents: Buffer = fs.readFileSync("./index.html");
+let indexContents: string = fs.readFileSync("./index.html", "utf8");
 const httpServer = http.createServer(function (req, res) {
 	// Write a response
 	res.writeHead(200, { "Content-type": "text/html" });
@@ -18,7 +23,11 @@ const httpServer = http.createServer(function (req, res) {
 })
 //const ws = new websocket::WebSocketServer({"httpServer":httpServer});
 
+// Test WASM
+// TODO: make WASM work with node
+//console.log("WASM check: 3 + 4 = " + mywasm.add(3, 4));
+
 // Run
-const PORT = 5005;
+const PORT: number = 5005;
 httpServer.listen(PORT, "127.0.0.1", () => console.log(`Server is open on port : ${PORT}`));
 
