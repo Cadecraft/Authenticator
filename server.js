de24e48ecc71bf4bd2a565979ae6a726fddcc1d9
Cadecraft <cadendotlee@gmail.com>
2024-07-30 14:12:57 -0700

    feat: practical use of rust

--- server.js
+++ server.js
@@ -6,51 +6,49 @@ var fs = require("fs");
 //import * as mywasm from "./wasm_modules/pkg";
 // Information
 var totalcount = 0;
-// Serving contents and setup
-var indexContents = fs.readFileSync("./index.html", "utf8");
-var httpServer = http.createServer(function (req, res) {
-    console.log(req.method + " request was made");
-    if (req.method == "POST") {
-        // Post
-        // Get the actual body
-        var bodyArr_1 = [];
-        req.on("data", function (chunk) {
-            bodyArr_1.push(chunk);
-        }).on("end", function () {
-            var body = JSON.parse(Buffer.concat(bodyArr_1).toString());
-            // At this point, `body` has the entire request body stored in it as a JSON
-            // Handle
-            totalcount++;
-            //console.log("- BODY: " + JSON.stringify(body));
-            console.log("- " + body.value + " (x" + body.count + ") (global x" + totalcount + ")");
-        });
-    }
-    else {
-        // Getting the page
-        // Update the page (this is unsafe and a very bad idea but funny to do)
-        // Start render region
-        var newIndex = indexContents.substring(0, indexContents.search("<!-- BEGIN Render Region! -->"));
-        // RENDER
-        newIndex += "<span style=\"color: cyan;\">" + totalcount + "</span>";
-        // END RENDER
-        // End render region
-        newIndex += indexContents.substring(indexContents.search("<!-- END Render Region! -->"));
-        // Write the response
-        res.writeHead(200, { "Content-type": "text/html" });
-        res.write(newIndex);
-        res.end();
-    }
-});
 // Test WASM
 // TODO: server-side rendering of the HTML and game and stuff
-//console.log("WASM check: 3 + 4 = " + mywasm.add(3, 4));
 var wasmBuffer = fs.readFileSync('./wasm_modules/pkg/wasm_modules_bg.wasm');
 WebAssembly.instantiate(wasmBuffer).then(function (wasmModule) {
     // Exported function live under instance.exports
     var add = wasmModule.instance.exports.add;
     var sum = add(5, 6);
-    console.log("Testing WASM: " + sum); // Outputs 11
+    console.log("Testing WASM (should output 11): " + sum); // Outputs 11
+    // Serving contents and setup
+    var indexContents = fs.readFileSync("./index.html", "utf8");
+    var httpServer = http.createServer(function (req, res) {
+        console.log(req.method + " request was made");
+        if (req.method == "POST") {
+            // Post
+            // Get the actual body
+            var bodyArr_1 = [];
+            req.on("data", function (chunk) {
+                bodyArr_1.push(chunk);
+            }).on("end", function () {
+                var body = JSON.parse(Buffer.concat(bodyArr_1).toString());
+                // At this point, `body` has the entire request body stored in it as a JSON
+                // Handle using the Rust/WebAssembly function!
+                totalcount = add(totalcount, 1);
+                console.log("- " + body.value + " (x" + body.count + ") (global x" + totalcount + ")");
+            });
+        }
+        else {
+            // Getting the page
+            // Update the page (this is a very bad idea but funny to do)
+            // Start render region
+            var newIndex = indexContents.substring(0, indexContents.search("<!-- BEGIN Render Region! -->"));
+            // RENDER
+            newIndex += "<span style=\"color: cyan;\">" + totalcount + "</span>";
+            // END RENDER
+            // End render region
+            newIndex += indexContents.substring(indexContents.search("<!-- END Render Region! -->"));
+            // Write the response
+            res.writeHead(200, { "Content-type": "text/html" });
+            res.write(newIndex);
+            res.end();
+        }
+    });
+    // Run
+    var PORT = 5005;
+    httpServer.listen(PORT, "127.0.0.1", function () { return console.log("Server is open on port : ".concat(PORT)); });
 });
-// Run
-var PORT = 5005;
-httpServer.listen(PORT, "127.0.0.1", function () { return console.log("Server is open on port : ".concat(PORT)); });
--- server.ts
+++ server.ts
@@ -6,53 +6,53 @@ import * as fs from "fs";
 // Information
 let totalcount: number = 0;
 
-// Serving contents and setup
-let indexContents: string = fs.readFileSync("./index.html", "utf8");
-const httpServer = http.createServer(function (req, res) {
-	console.log(req.method + " request was made");
-	if (req.method == "POST") {
-		// Post
-		// Get the actual body
-		let bodyArr = [];
-		req.on("data", chunk => {
-			bodyArr.push(chunk);
-		}).on("end", () => {
-			let body = JSON.parse(Buffer.concat(bodyArr).toString());
-			// At this point, `body` has the entire request body stored in it as a JSON
-			// Handle
-			totalcount++;
-			//console.log("- BODY: " + JSON.stringify(body));
-			console.log("- " + body.value + " (x" + body.count + ") (global x" + totalcount + ")");
-		});
-	} else {
-		// Getting the page
-		// Update the page (this is a very bad idea but funny to do)
-		// Start render region
-		let newIndex = indexContents.substring(0, indexContents.search("<!-- BEGIN Render Region! -->"));
-		// RENDER
-		newIndex += "<span style=\"color: cyan;\">" + totalcount + "</span>";
-		// END RENDER
-		// End render region
-		newIndex += indexContents.substring(indexContents.search("<!-- END Render Region! -->"));
-
-		// Write the response
-		res.writeHead(200, { "Content-type": "text/html" });
-		res.write(newIndex);
-		res.end();
-	}
-})
-
 // Test WASM
 // TODO: server-side rendering of the HTML and game and stuff
-//console.log("WASM check: 3 + 4 = " + mywasm.add(3, 4));
 const wasmBuffer = fs.readFileSync('./wasm_modules/pkg/wasm_modules_bg.wasm');
 WebAssembly.instantiate(wasmBuffer).then(wasmModule => {
+
 	// Exported function live under instance.exports
 	const add = wasmModule.instance.exports.add as CallableFunction;
 	const sum = add(5, 6);
-	console.log("Testing WASM: " + sum); // Outputs 11
-});
+	console.log("Testing WASM (should output 11): " + sum); // Outputs 11
 
-// Run
-const PORT: number = 5005;
-httpServer.listen(PORT, "127.0.0.1", () => console.log(`Server is open on port : ${PORT}`));
+	// Serving contents and setup
+	let indexContents: string = fs.readFileSync("./index.html", "utf8");
+	const httpServer = http.createServer(function (req, res) {
+		console.log(req.method + " request was made");
+		if (req.method == "POST") {
+			// Post
+			// Get the actual body
+			let bodyArr = [];
+			req.on("data", chunk => {
+				bodyArr.push(chunk);
+			}).on("end", () => {
+				let body = JSON.parse(Buffer.concat(bodyArr).toString());
+				// At this point, `body` has the entire request body stored in it as a JSON
+				// Handle using the Rust/WebAssembly function!
+				totalcount = add(totalcount, 1);
+				console.log("- " + body.value + " (x" + body.count + ") (global x" + totalcount + ")");
+			});
+		} else {
+			// Getting the page
+			// Update the page (this is a very bad idea but funny to do)
+			// Start render region
+			let newIndex = indexContents.substring(0, indexContents.search("<!-- BEGIN Render Region! -->"));
+			// RENDER
+			newIndex += "<span style=\"color: cyan;\">" + totalcount + "</span>";
+			// END RENDER
+			// End render region
+			newIndex += indexContents.substring(indexContents.search("<!-- END Render Region! -->"));
+
+			// Write the response
+			res.writeHead(200, { "Content-type": "text/html" });
+			res.write(newIndex);
+			res.end();
+		}
+	})
+
+	// Run
+	const PORT: number = 5005;
+	httpServer.listen(PORT, "127.0.0.1", () => console.log(`Server is open on port : ${PORT}`));
+
+});
