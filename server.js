2465de54feb5c5e742e4174020fc6da06dfec9fa
Cadecraft <cadendotlee@gmail.com>
2024-07-30 08:34:49 -0700

    feat: dark mode, wasm attempt

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 // Requires
+//import http = require("http");
 var http = require("http");
+//import fs = require("fs");
 var fs = require("fs");
+//import path = require("fs");
+//import websocket = require("websocket");
+//import * as mywasm from "./wasm_modules/pkg"; // Include final `/`?
 //import server as WebSocketServer from "websocket";
+// Needed this to be the JS for node to be happy:
+//var mywasm = import("./wasm_modules/pkg/wasm_modules");
 // Information
 // TODO: start storing info
 // Serving contents and setup
-var indexContents = fs.readFileSync("./index.html");
+var indexContents = fs.readFileSync("./index.html", "utf8");
 var httpServer = http.createServer(function (req, res) {
     // Write a response
     res.writeHead(200, { "Content-type": "text/html" });
@@ -15,6 +22,9 @@ var httpServer = http.createServer(function (req, res) {
     res.end();
 });
 //const ws = new websocket::WebSocketServer({"httpServer":httpServer});
+// Test WASM
+// TODO: make WASM work with node
+//console.log("WASM check: 3 + 4 = " + mywasm.add(3, 4));
 // Run
 var PORT = 5005;
 httpServer.listen(PORT, "127.0.0.1", function () { return console.log("Server is open on port : ".concat(PORT)); });
